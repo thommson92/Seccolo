8120fe70aaa2371ac005b609bb1d5efc5b50ea7f
Thomas Kellner <thomas-kellner@web.de>
2025-01-06 16:56:04 +0100

    1.) Moved Activities to separate JSON file 2.) Asynchronously read in the activities at the beginning of the page (index.js), hand this variable over as a sessionStorage and again read it in in game.js. 3.) Updated to 2025

--- js/index.js
+++ js/index.js
@@ -3,3 +3,29 @@ function updatePlayerCount(value) {
     document.getElementById('playerCount').textContent = value + (value === '1' ? ' Spieler' : ' Spieler');
     document.getElementById('hiddenPlayerInput').value = value;
 }
+
+async function loadActivities() {
+    try {
+        const response = await fetch('./js/activities.json');
+        console.log(response)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const activities = await response.json();
+        return activities; // Returns the loaded activities
+    } catch (error) {
+        console.error('Error fetching activities:', error);
+        return null; // Return null in case of an error
+    }
+}
+
+loadActivities().then(activities => {
+    if (activities) {
+        // Proceed with initializing the game
+        sessionStorage.setItem('activities', JSON.stringify(activities));
+        console.log(activities)
+        console.log(activities.length)
+    } else {
+        console.error('Failed to load activities.');
+    }
+});
